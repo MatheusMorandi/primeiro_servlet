b6981cfd2fa27bba6e4c56fdbaf288b57a007e23
MatheusMorandi <theu.morandi@gmail.com>
2023-04-18 18:41:54 +0000

    New

---
+++ app/src/main/java/exservlets/PrimeiroServlet.java
@@ -0,0 +1,31 @@
+package exservlets;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.annotation.WebServlet;
+
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "IndexServlet", urlPatterns = "/")
+public class PrimeiroServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse  response)
+        throws ServletException, IOException{
+
+            String n = request.getParameter("nome");
+
+            request.setAttribute("nome", n);
+
+            //response.getWriter().print("Olá " + n);
+
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+    }
+    
+}
